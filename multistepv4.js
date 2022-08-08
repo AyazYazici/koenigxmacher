8203f8c9efc88dbe36e89cd3a767981a0712ef48
AyazYazici <110823045+AyazYazici@users.noreply.github.com>
2022-08-08 13:57:03 +0200

    Add files via upload

---
+++ multistepv4.js
@@ -0,0 +1,386 @@
+var projekt, Radio_4, seitens, komplex, design, editor, seiten, produkte, beratung, zusatz, zeit, next_tab;
+
+
+var we_tabs_next_button = '[bloc=next-q]';
+    var we_tabs_prev_button = '[bloc=prev-q]';
+    var we_tabs_active_class = 'active';
+
+    var we_activeTab,we_indexOfActiveTab, we_indexOfNextTab, we_indexOfPrevTab, we_prevTab, we_amountOfTabs, nextTab;
+
+    var tabList = ['projekt'];
+
+    openTab(tabList[0]);
+
+    $(we_tabs_next_button).on("click", function () {
+      var activeTab = $(".w--tab-active").attr("data-w-tab");
+      var indexOfActiveTab = tabList.indexOf(activeTab);
+      var indexOfNextTab = indexOfActiveTab + 1;
+      var nextTab = tabList[indexOfNextTab];
+      var amountOfTabs = tabList.length;
+      var attrNextTab = $('.w--tab-active').attr('next-tab');
+
+      openTab(attrNextTab);
+
+      attrNextTab = $('.w--tab-active').attr('next-tab');
+
+      if (typeof attrNextTab !== 'undefined' && attrNextTab !== false) {
+        $(we_tabs_next_button).addClass(we_tabs_active_class);
+      } else {
+        $(we_tabs_next_button).removeClass(we_tabs_active_class);
+      }
+
+      if (amountOfTabs > 1) {
+        $(we_tabs_prev_button).addClass(we_tabs_active_class);
+      }
+    });
+
+    $(we_tabs_prev_button).on("click", function () {
+      var activeTab = $(".w--tab-active").attr("data-w-tab");
+      var indexOfActiveTab = tabList.indexOf(activeTab);
+      var indexOfPrevTab = indexOfActiveTab - 1;
+      var prevTab = tabList[indexOfPrevTab];
+
+      var attrPrevTab = $('.w--tab-active').attr('prev-tab');
+
+      if (indexOfActiveTab > 0) {
+        openTab(attrPrevTab);
+        $(we_tabs_next_button).addClass(we_tabs_active_class);
+      }
+
+      if (indexOfActiveTab <= 1) {
+        $(we_tabs_prev_button).removeClass(we_tabs_active_class);
+      } else {
+        $(we_tabs_prev_button).addClass(we_tabs_active_class);
+      }
+    });
+
+    function openTab(tab) {
+      var tab_link = "a[data-w-tab=" + tab;
+      tab = "div[data-w-tab=" + tab;
+
+      $(tab).siblings().removeClass("w--tab-active");
+      $(tab).addClass("w--tab-active");
+      $(tab_link).siblings("a").removeClass("w--current");
+      $(tab_link).addClass("w--current");
+    }$("[name=Radio_4]").on("input", function () {
+    Radio_4 = getValueFromInput("Radio_4");
+      if (Radio_4 == 'webseitenn') {
+
+        nextTab = 'seiten';
+        we_activeTab = $(".w--tab-active").attr("data-w-tab");
+        we_indexOfActiveTab = tabList.indexOf(we_activeTab);
+        we_indexOfNextTab = we_indexOfActiveTab + 1;
+        we_indexOfPrevTab = we_indexOfActiveTab - 1;
+        we_prevTab = tabList[we_indexOfPrevTab];
+        we_amountOfTabs = tabList.length;
+
+        if (we_indexOfNextTab < we_amountOfTabs) {
+          tabList[we_indexOfNextTab] = nextTab;
+        } else {
+          tabList.push(nextTab);
+        }
+        $(we_tabs_next_button).addClass(we_tabs_active_class);
+        $(".w--tab-active").attr('next-tab',nextTab);
+        $($('[data-w-tab=seiten]')).attr('prev-tab',we_activeTab);
+
+        } else if (Radio_4 == 'onlineshopprojekt') {
+
+        nextTab = 'produkte';
+        we_activeTab = $(".w--tab-active").attr("data-w-tab");
+        we_indexOfActiveTab = tabList.indexOf(we_activeTab);
+        we_indexOfNextTab = we_indexOfActiveTab + 1;
+        we_indexOfPrevTab = we_indexOfActiveTab - 1;
+        we_prevTab = tabList[we_indexOfPrevTab];
+        we_amountOfTabs = tabList.length;
+
+        if (we_indexOfNextTab < we_amountOfTabs) {
+          tabList[we_indexOfNextTab] = nextTab;
+        } else {
+          tabList.push(nextTab);
+        }
+        $(we_tabs_next_button).addClass(we_tabs_active_class);
+        $(".w--tab-active").attr('next-tab',nextTab);
+        $($('[data-w-tab=produkte]')).attr('prev-tab',we_activeTab);
+
+        } else if (Radio_4 == 'indieprojekt') {
+
+        nextTab = 'beratung';
+        we_activeTab = $(".w--tab-active").attr("data-w-tab");
+        we_indexOfActiveTab = tabList.indexOf(we_activeTab);
+        we_indexOfNextTab = we_indexOfActiveTab + 1;
+        we_indexOfPrevTab = we_indexOfActiveTab - 1;
+        we_prevTab = tabList[we_indexOfPrevTab];
+        we_amountOfTabs = tabList.length;
+
+        if (we_indexOfNextTab < we_amountOfTabs) {
+          tabList[we_indexOfNextTab] = nextTab;
+        } else {
+          tabList.push(nextTab);
+        }
+        $(we_tabs_next_button).addClass(we_tabs_active_class);
+        $(".w--tab-active").attr('next-tab',nextTab);
+        $($('[data-w-tab=beratung]')).attr('prev-tab',we_activeTab);
+
+        }
+});
+
+    $("[name=Radio_4]").parent("label.w-radio").on("click", function () {
+      clickedRadioButtonValue = $("input", this).val();
+      Radio_4 = getValueFromInput("Radio_4");
+        if (Radio_4 == 'webseitenn') {
+
+        nextTab = 'seiten';
+        we_activeTab = $(".w--tab-active").attr("data-w-tab");
+        we_indexOfActiveTab = tabList.indexOf(we_activeTab);
+        we_indexOfNextTab = we_indexOfActiveTab + 1;
+        we_indexOfPrevTab = we_indexOfActiveTab - 1;
+        we_prevTab = tabList[we_indexOfPrevTab];
+        we_amountOfTabs = tabList.length;
+
+        if (we_indexOfNextTab < we_amountOfTabs) {
+          tabList[we_indexOfNextTab] = nextTab;
+        } else {
+          tabList.push(nextTab);
+        }
+        $(we_tabs_next_button).addClass(we_tabs_active_class);
+        $(".w--tab-active").attr('next-tab',nextTab);
+        $($('[data-w-tab=seiten]')).attr('prev-tab',we_activeTab);
+
+        } else if (Radio_4 == 'onlineshopprojekt') {
+
+        nextTab = 'produkte';
+        we_activeTab = $(".w--tab-active").attr("data-w-tab");
+        we_indexOfActiveTab = tabList.indexOf(we_activeTab);
+        we_indexOfNextTab = we_indexOfActiveTab + 1;
+        we_indexOfPrevTab = we_indexOfActiveTab - 1;
+        we_prevTab = tabList[we_indexOfPrevTab];
+        we_amountOfTabs = tabList.length;
+
+        if (we_indexOfNextTab < we_amountOfTabs) {
+          tabList[we_indexOfNextTab] = nextTab;
+        } else {
+          tabList.push(nextTab);
+        }
+        $(we_tabs_next_button).addClass(we_tabs_active_class);
+        $(".w--tab-active").attr('next-tab',nextTab);
+        $($('[data-w-tab=produkte]')).attr('prev-tab',we_activeTab);
+
+        } else if (Radio_4 == 'indieprojekt') {
+
+        nextTab = 'beratung';
+        we_activeTab = $(".w--tab-active").attr("data-w-tab");
+        we_indexOfActiveTab = tabList.indexOf(we_activeTab);
+        we_indexOfNextTab = we_indexOfActiveTab + 1;
+        we_indexOfPrevTab = we_indexOfActiveTab - 1;
+        we_prevTab = tabList[we_indexOfPrevTab];
+        we_amountOfTabs = tabList.length;
+
+        if (we_indexOfNextTab < we_amountOfTabs) {
+          tabList[we_indexOfNextTab] = nextTab;
+        } else {
+          tabList.push(nextTab);
+        }
+        $(we_tabs_next_button).addClass(we_tabs_active_class);
+        $(".w--tab-active").attr('next-tab',nextTab);
+        $($('[data-w-tab=beratung]')).attr('prev-tab',we_activeTab);
+
+        }
+});
+    $("[name=seitens]").on("input", function () {
+    seitens = getValueFromInput("seitens");
+
+      nextTab = 'design';
+      we_activeTab = $(".w--tab-active").attr("data-w-tab");
+      we_indexOfActiveTab = tabList.indexOf(we_activeTab);
+      we_indexOfNextTab = we_indexOfActiveTab + 1;
+      we_indexOfPrevTab = we_indexOfActiveTab - 1;
+      we_prevTab = tabList[we_indexOfPrevTab];
+      we_amountOfTabs = tabList.length;
+
+      if (we_indexOfNextTab < we_amountOfTabs) {
+        tabList[we_indexOfNextTab] = nextTab;
+      } else {
+        tabList.push(nextTab);
+      }
+      $(we_tabs_next_button).addClass(we_tabs_active_class);
+      $(".w--tab-active").attr('next-tab',nextTab);
+      $($('[data-w-tab=design]')).attr('prev-tab',we_activeTab);
+
+      });
+
+    $("[name=seitens]").parent("label.w-radio").on("click", function () {
+      clickedRadioButtonValue = $("input", this).val();
+      seitens = getValueFromInput("seitens");
+
+      nextTab = 'design';
+      we_activeTab = $(".w--tab-active").attr("data-w-tab");
+      we_indexOfActiveTab = tabList.indexOf(we_activeTab);
+      we_indexOfNextTab = we_indexOfActiveTab + 1;
+      we_indexOfPrevTab = we_indexOfActiveTab - 1;
+      we_prevTab = tabList[we_indexOfPrevTab];
+      we_amountOfTabs = tabList.length;
+
+      if (we_indexOfNextTab < we_amountOfTabs) {
+        tabList[we_indexOfNextTab] = nextTab;
+      } else {
+        tabList.push(nextTab);
+      }
+      $(we_tabs_next_button).addClass(we_tabs_active_class);
+      $(".w--tab-active").attr('next-tab',nextTab);
+      $($('[data-w-tab=design]')).attr('prev-tab',we_activeTab);
+
+      });
+    $("[name=komplex]").on("input", function () {
+    komplex = getValueFromInput("komplex");
+
+      nextTab = 'zusatz';
+      we_activeTab = $(".w--tab-active").attr("data-w-tab");
+      we_indexOfActiveTab = tabList.indexOf(we_activeTab);
+      we_indexOfNextTab = we_indexOfActiveTab + 1;
+      we_indexOfPrevTab = we_indexOfActiveTab - 1;
+      we_prevTab = tabList[we_indexOfPrevTab];
+      we_amountOfTabs = tabList.length;
+
+      if (we_indexOfNextTab < we_amountOfTabs) {
+        tabList[we_indexOfNextTab] = nextTab;
+      } else {
+        tabList.push(nextTab);
+      }
+      $(we_tabs_next_button).addClass(we_tabs_active_class);
+      $(".w--tab-active").attr('next-tab',nextTab);
+      $($('[data-w-tab=zusatz]')).attr('prev-tab',we_activeTab);
+
+      });
+
+    $("[name=komplex]").parent("label.w-radio").on("click", function () {
+      clickedRadioButtonValue = $("input", this).val();
+      komplex = getValueFromInput("komplex");
+
+      nextTab = 'zusatz';
+      we_activeTab = $(".w--tab-active").attr("data-w-tab");
+      we_indexOfActiveTab = tabList.indexOf(we_activeTab);
+      we_indexOfNextTab = we_indexOfActiveTab + 1;
+      we_indexOfPrevTab = we_indexOfActiveTab - 1;
+      we_prevTab = tabList[we_indexOfPrevTab];
+      we_amountOfTabs = tabList.length;
+
+      if (we_indexOfNextTab < we_amountOfTabs) {
+        tabList[we_indexOfNextTab] = nextTab;
+      } else {
+        tabList.push(nextTab);
+      }
+      $(we_tabs_next_button).addClass(we_tabs_active_class);
+      $(".w--tab-active").attr('next-tab',nextTab);
+      $($('[data-w-tab=zusatz]')).attr('prev-tab',we_activeTab);
+
+      });
+    $("[name=editor]").on("input", function () {
+    editor = getValueFromInput("editor");
+
+      nextTab = 'zusatz';
+      we_activeTab = $(".w--tab-active").attr("data-w-tab");
+      we_indexOfActiveTab = tabList.indexOf(we_activeTab);
+      we_indexOfNextTab = we_indexOfActiveTab + 1;
+      we_indexOfPrevTab = we_indexOfActiveTab - 1;
+      we_prevTab = tabList[we_indexOfPrevTab];
+      we_amountOfTabs = tabList.length;
+
+      if (we_indexOfNextTab < we_amountOfTabs) {
+        tabList[we_indexOfNextTab] = nextTab;
+      } else {
+        tabList.push(nextTab);
+      }
+      $(we_tabs_next_button).addClass(we_tabs_active_class);
+      $(".w--tab-active").attr('next-tab',nextTab);
+      $($('[data-w-tab=zusatz]')).attr('prev-tab',we_activeTab);
+
+      });
+
+    $("[name=editor]").parent("label.w-radio").on("click", function () {
+      clickedRadioButtonValue = $("input", this).val();
+      editor = getValueFromInput("editor");
+
+      nextTab = 'zusatz';
+      we_activeTab = $(".w--tab-active").attr("data-w-tab");
+      we_indexOfActiveTab = tabList.indexOf(we_activeTab);
+      we_indexOfNextTab = we_indexOfActiveTab + 1;
+      we_indexOfPrevTab = we_indexOfActiveTab - 1;
+      we_prevTab = tabList[we_indexOfPrevTab];
+      we_amountOfTabs = tabList.length;
+
+      if (we_indexOfNextTab < we_amountOfTabs) {
+        tabList[we_indexOfNextTab] = nextTab;
+      } else {
+        tabList.push(nextTab);
+      }
+      $(we_tabs_next_button).addClass(we_tabs_active_class);
+      $(".w--tab-active").attr('next-tab',nextTab);
+      $($('[data-w-tab=zusatz]')).attr('prev-tab',we_activeTab);
+
+      });
+    $("[name=zeit]").on("input", function () {
+    zeit = getValueFromInput("zeit");
+
+      nextTab = 'zusatz';
+      we_activeTab = $(".w--tab-active").attr("data-w-tab");
+      we_indexOfActiveTab = tabList.indexOf(we_activeTab);
+      we_indexOfNextTab = we_indexOfActiveTab + 1;
+      we_indexOfPrevTab = we_indexOfActiveTab - 1;
+      we_prevTab = tabList[we_indexOfPrevTab];
+      we_amountOfTabs = tabList.length;
+
+      if (we_indexOfNextTab < we_amountOfTabs) {
+        tabList[we_indexOfNextTab] = nextTab;
+      } else {
+        tabList.push(nextTab);
+      }
+      $(we_tabs_next_button).addClass(we_tabs_active_class);
+      $(".w--tab-active").attr('next-tab',nextTab);
+      $($('[data-w-tab=zusatz]')).attr('prev-tab',we_activeTab);
+
+      });
+
+    $("[name=zeit]").parent("label.w-radio").on("click", function () {
+      clickedRadioButtonValue = $("input", this).val();
+      zeit = getValueFromInput("zeit");
+
+      nextTab = 'zusatz';
+      we_activeTab = $(".w--tab-active").attr("data-w-tab");
+      we_indexOfActiveTab = tabList.indexOf(we_activeTab);
+      we_indexOfNextTab = we_indexOfActiveTab + 1;
+      we_indexOfPrevTab = we_indexOfActiveTab - 1;
+      we_prevTab = tabList[we_indexOfPrevTab];
+      we_amountOfTabs = tabList.length;
+
+      if (we_indexOfNextTab < we_amountOfTabs) {
+        tabList[we_indexOfNextTab] = nextTab;
+      } else {
+        tabList.push(nextTab);
+      }
+      $(we_tabs_next_button).addClass(we_tabs_active_class);
+      $(".w--tab-active").attr('next-tab',nextTab);
+      $($('[data-w-tab=zusatz]')).attr('prev-tab',we_activeTab);
+
+      });
+    $('[bloc=next-q]').on('click',function() {
+  if($('.w--tab-active').attr('data-w-tab')=='zusatz') {
+
+        nextTab = 'next_tab';
+        we_activeTab = $(".w--tab-active").attr("data-w-tab");
+        we_indexOfActiveTab = tabList.indexOf(we_activeTab);
+        we_indexOfNextTab = we_indexOfActiveTab + 1;
+        we_indexOfPrevTab = we_indexOfActiveTab - 1;
+        we_prevTab = tabList[we_indexOfPrevTab];
+        we_amountOfTabs = tabList.length;
+
+        if (we_indexOfNextTab < we_amountOfTabs) {
+          tabList[we_indexOfNextTab] = nextTab;
+        } else {
+          tabList.push(nextTab);
+        }
+        $(we_tabs_next_button).addClass(we_tabs_active_class);
+        $(".w--tab-active").attr('next-tab',nextTab);
+        $($('[data-w-tab=next_tab]')).attr('prev-tab',we_activeTab);
+
+
+      }});
